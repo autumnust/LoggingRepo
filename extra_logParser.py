42c323f2330cfe96cb5c4e200f551b9ad2a9e1e1
autumnust <autumnust@gmail.com>
2016-11-01 16:49:18 -0400

    contains all partitions' timestamp

--- extra_logParser.py
+++ extra_logParser.py
@@ -61,7 +61,7 @@ Starting from: xxxxx
 Ending at: xxxxx
 '''
 # TODO: A verbose mode to output each partition's time for that transformation. 
-def output_stage_info(stage_info, stage_id, output, stage_id_name_map, rdd_id_name_map): 
+def output_stage_info(stage_info, stage_id, output, stage_id_name_map, rdd_id_name_map, verbose): 
 	rdd_list = stage_info[stage_id]
 	output.write("Stage_"+ str(stage_id) + "(" + str(len(rdd_list)) + "):" + stage_id_name_map[stage_id] + str(stage_id) + "\n")
 
@@ -74,49 +74,82 @@ def output_stage_info(stage_info, stage_id, output, stage_id_name_map, rdd_id_na
 		else: 
 			rdd_real_name = rdd_id_name_map[rdd_id]
 		output.write("\t - RDD_" + str(rdd_id) + ":Generating " + rdd_real_name + "(" + str(len(part_list)) + ")\n")
-		[straggler_id, start_time, end_time] = getSlowestPartitionTime(part_list)
-		output.write("\t\t Start:" + str(milli2Readable(start_time)) + " End: " + str(milli2Readable(end_time)) \
-					+ "  Lasting: " + str(end_time - start_time ) + "ms" + "\n"
-					) 		
+		if ( verbose == 0 ): 
+			[straggler_id, start_time, end_time] = getSlowestPartitionTime(part_list)
+			output.write("\t\t Start:" + str(milli2Readable(start_time)) + " End: " + str(milli2Readable(end_time)) \
+						+ "  Lasting: " + str(end_time - start_time ) + "ms" + "\n"
+						) 	
+		else:
+			for part_id, time_interval in part_list.iteritems(): 				
+				output.write("\t\tPartition_" + str(part_id) + " start from :" + str(milli2Readable(time_interval[0]))\
+					+  " , end at: " + str(milli2Readable(time_interval[1])) + ", Lasting : " \
+					+ str(long(time_interval[1]) - long(time_interval[0])) +  "ms  \n")
+		# Print all partitions' time.	
 	output.write("\n\n")
 
-	
-# Main program
-with open (output, 'w') as f : 
-	with open(filename, 'r') as fd : 
-		for line in fd:
-			# ***  Parsing  ***
-			# First 36 characters, just strip it. 
-			line = line[37:]
-			split_result = line.split(",") 
-			rdd_name = ""
-			rdd_id = ""
-			# task_type can be: ShuffleMapTask, ResultTask, iterator.FromParent, iterator.FromCache
-			if (len(split_result) == 4 ):
-				[task_type, task_attmpt_id, part_id, stage_id] = split_result
-			elif ( len(split_result) == 6): 
-				[task_type, rdd_id, rdd_name, task_attmpt_id, part_id, stage_id] = split_result			
 
-			stage_id = int(stage_id.split(":")[1])
-			part_id = int(part_id[(part_id.find(":")+1):]) 		
-			task_attmpt_id = int(task_attmpt_id[(task_attmpt_id.find(":")+1):])
-			task_name = task_type.split("]")[0]
-			# End or start is not important, just make sure each operation's time interval has two elements. 
-			timestamp = long(task_type.split("]")[1].split(":")[1])
+def main(argv):
+	# This part is put to enable the verbose. 
+	inputfile = ''
+	outputfile = ''
+	try:
+  		opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+	except getopt.GetoptError:
+		print 'test.py -i <inputfile> -o <outputfile>'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'extra_logParser.py -i <inputfile> -o <outputfile>'
+			sys.exit()
+		elif opt in ("-i", "--ifile"):
+			inputfile = arg
+		elif opt in ("-o", "--ofile"):
+			outputfile = arg
+	print 'Input file is "', inputfile
+	print 'Output file is "', outputfile
 
-			if (len(split_result) == 6 ): 
-				rdd_name = rdd_name.split(":")[1]
-				rdd_id = int(rdd_id.split(":")[1])
-				rdd_id_name_map[rdd_id] = rdd_name
-				# ***  Info Assemble  ***
-				stage_info[stage_id][rdd_id][part_id].append(timestamp)						
-			elif (len(split_result) == 4): 
-				stage_id_name_map[stage_id] = task_name
-				stage_time_map[stage_id][part_id].append(timestamp)
-	
-	# Output all the stage. 
-	for stage_id, rdd_list in stage_info.iteritems(): 
-		output_stage_info( stage_info, stage_id, f, stage_id_name_map, rdd_id_name_map )
+	# Switch to control if each partitions' time can be output. 
+	verbose = 1 
 
-	fd.close()
-f.close()
+	# Main program
+	with open (output, 'w') as f : 
+		with open(filename, 'r') as fd : 
+			for line in fd:
+				# ***  Parsing  ***
+				# First 36 characters, just strip it. 
+				line = line[37:]
+				split_result = line.split(",") 
+				rdd_name = ""
+				rdd_id = ""
+				# task_type can be: ShuffleMapTask, ResultTask, iterator.FromParent, iterator.FromCache
+				if (len(split_result) == 4 ):
+					[task_type, task_attmpt_id, part_id, stage_id] = split_result
+				elif ( len(split_result) == 6): 
+					[task_type, rdd_id, rdd_name, task_attmpt_id, part_id, stage_id] = split_result			
+
+				stage_id = int(stage_id.split(":")[1])
+				part_id = int(part_id[(part_id.find(":")+1):]) 		
+				task_attmpt_id = int(task_attmpt_id[(task_attmpt_id.find(":")+1):])
+				task_name = task_type.split("]")[0]
+				# End or start is not important, just make sure each operation's time interval has two elements. 
+				timestamp = long(task_type.split("]")[1].split(":")[1])
+
+				if (len(split_result) == 6 ): 
+					rdd_name = rdd_name.split(":")[1]
+					rdd_id = int(rdd_id.split(":")[1])
+					rdd_id_name_map[rdd_id] = rdd_name
+					# ***  Info Assemble  ***
+					stage_info[stage_id][rdd_id][part_id].append(timestamp)						
+				elif (len(split_result) == 4): 
+					stage_id_name_map[stage_id] = task_name
+					stage_time_map[stage_id][part_id].append(timestamp)
+		
+		# Output all the stage. 
+		for stage_id, rdd_list in stage_info.iteritems(): 
+			output_stage_info( stage_info, stage_id, f, stage_id_name_map, rdd_id_name_map, verbose)
+
+		fd.close()
+	f.close()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])	
